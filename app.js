fa88e8d0b2b2b5aa60c57232e86e0924d68ade74
Prateet Tiwari <prateettiwari29@gmail.com>
2026-01-12 04:52:29 +0530

    feat: add CORS middleware for handling cross-origin requests

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import healthRoute from './routes/health.route.js';
 import authRoute from './routes/auth.route.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api/health', healthRoute);
 app.use('/api/auth', authRoute);
