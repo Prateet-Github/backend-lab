4162b49b94496c46a2ebbf9519e61f2877b1cb67
Prateet Tiwari <prateettiwari29@gmail.com>
2026-02-13 02:16:02 +0530

    feat: implement Docker setup with nginx and multiple app instances

--- node-nginx/server.js
+++ node-nginx/server.js
@@ -3,11 +3,12 @@ import express from 'express';
 const app = express();
 
 app.get('/', (_req, res) => {
-  res.send('Hello from Node.js  server!');
+res.send(`Hello from instance ${process.env.INSTANCE_ID || 'unknown'}`);
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = 5001;
+const HOST = '0.0.0.0';
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT,HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
