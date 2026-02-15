7009ac0f37d1f3edd67be192e99653a0e71d6ed5
Prateet Tiwari <prateettiwari29@gmail.com>
2026-02-15 11:50:04 +0530

    fix: update response message in server to be more descriptive

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,7 +3,7 @@ import http from 'http';
 const server = http.createServer((_req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hello, from Node.js!\n');
 });
 
 const PORT = process.env.PORT || 3000;
