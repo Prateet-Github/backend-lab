98b41e9d4434cf45b76387d7b18ae482660994c6
Prateet Tiwari <prateettiwari29@gmail.com>
2026-02-20 21:30:39 +0530

    feat(server): implement GET /users endpoint to return user list in JSON format

--- core-nodejs/server.js
+++ core-nodejs/server.js
@@ -3,8 +3,18 @@ import http from "http";
 const PORT = 5001;
 const HOST = '127.0.0.1';
 
-const server = http.createServer((_req,res) => {
-  res.end("Node.js Server");
+let users = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' },
+  { id: 3, name: 'Charlie' },
+];
+
+const server = http.createServer((req,res) => {
+  if(req.method === "GET" && req.url === '/users'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(users));
+  }
 });
 
 server.listen(PORT, HOST, () => {
