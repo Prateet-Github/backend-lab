ec2d3d9f7b4bc6ebe58b9dd4d0ff47ef74cb62b8
Prateet Tiwari <prateettiwari29@gmail.com>
2026-02-24 00:27:40 +0530

    feat(streams): update server to demonstrate streams usage

--- streams/server.js
+++ streams/server.js
@@ -1,9 +1,17 @@
 import http from 'http';
+import fs from 'fs';
 
 const PORT = 3000;
 
 const server = http.createServer((_req,res) => {
-res.end('Node.js streams.');
+
+// without streams  
+// const file = fs.readFileSync('sample.txt');
+
+// with streams
+const readableStream = fs.createReadStream('sample.txt');
+readableStream.pipe(res)
+
 });
 
 server.listen(PORT, () => {
