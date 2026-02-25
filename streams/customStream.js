84d3ea2298f37f2a81fd5774424037aeb68125bf
Prateet Tiwari <prateettiwari29@gmail.com>
2026-02-25 11:40:04 +0530

    Readble, Writable streams done

--- streams/customStream.js
+++ streams/customStream.js
@@ -1,11 +1,21 @@
-import {Readable} from 'stream';
+import {Readable, Writable} from 'stream';
+
+const writableStream = new Writable({
+  write (s){
+    console.log('Writing', s.toString());
+  }
+})
 
 const readableStream = new Readable({
+  highWaterMark: 1024 * 1024, // Buffer limit of a stream
   read (){}
 });
 
-readableStream.on('data', (chunk)=>{
-  console.log('Data is:', chunk);
+readableStream.on('data', (chunk) => {
+  console.log('Data is:', chunk.toString()); 
+  // Data is: <Buffer 4c 65 61 72 6e 69 6e 67 20 74 68 65 20 73 74 72 65 61 6d 73 20 69 6e 20 4e 6f 64 65 2e 6a 73>
+  // Data is: Learning the streams in Node.js (.toString())
+  writableStream.write(chunk);
 });
 
-readableStream.push('Learning the streams in Node.js');
+console.log(readableStream.push('Learning the streams in Node.js'));
