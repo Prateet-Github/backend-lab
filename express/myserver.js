eb7c97d718a741868a7d15c585c4731a18d77998
Prateet Tiwari <prateettiwari29@gmail.com>
2026-02-26 15:09:15 +0530

    feat(users): initialize user data and enhance user retrieval endpoint

--- express/myserver.js
+++ express/myserver.js
@@ -3,10 +3,21 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-let users = [];
+let users = [
+  {id:1,name:"one",email:"1@mail.com"},
+  {id:2,name:"two",email:"2@mail.com"},
+  {id:3,name:"three",email:"3@mail.com"}
 
-app.get('/',(_req,res)=>{
-res.status(200).json(users)
+];
+
+app.get('/:id',(req,res)=>{
+  const withId = req.params.id
+  const user = users.find(user => user.id == withId)
+  if(user){
+    res.status(200).json(user)
+  }else{
+    res.status(404).json({message:"User not found"})
+  }
 })
 
 app.post('/',(req,res)=>{
