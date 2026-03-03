238f1d68cb9aa7e8abe97d6fd1aaa63f488e47c4
Prateet Tiwari <prateettiwari29@gmail.com>
2026-03-04 00:14:49 +0530

    feat(api-gateway): implement proxy middleware for auth and users routes

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import {createProxyMiddleware} from 'http-proxy-middleware';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello from API Gateway!');
-});
+app.use("/auth", createProxyMiddleware({
+  target: "http://localhost:3001", 
+  changeOrigin: true
+}));
+
+app.use("/users", createProxyMiddleware({
+  target: "http://localhost:3002", 
+  changeOrigin: true
+}));
 
 app.listen(PORT, () => {
   console.log(`API Gateway is running on http://localhost:${PORT}`);
