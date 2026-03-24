3f1f58f717036c3b76327e68bb13142e385daa5a
Prateet Tiwari <prateettiwari29@gmail.com>
2026-03-24 14:38:19 +0530

    feat(server): implement SSE endpoint to stream time data

--- sse/server.js
+++ sse/server.js
@@ -1,11 +1,31 @@
 import express from 'express';
 
 const app = express();
-
 const PORT = 5001;
 
 app.get('/', (_req, res) => {
   res.send('Hello from the server!');
+});
+
+app.get('/events', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  res.write(`data: Connected to SSE\n\n`);
+
+  const interval = setInterval(() => {
+    const data = {
+      time: new Date().toISOString(),
+    };
+
+    res.write(`data: ${JSON.stringify(data)}\n\n`);
+  }, 2000);
+
+  req.on('close', () => {
+    clearInterval(interval);
+    res.end();
+  });
 });
 
 app.listen(PORT, () => {
